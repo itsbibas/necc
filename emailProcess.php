5852475172b235b830bc987310bbc1ff19d00910
iamdurlove <durlove60@gmail.com>
2022-08-18 22:08:41 +0545

    final contact form update

--- emailProcess.php
+++ emailProcess.php
@@ -7,6 +7,8 @@ if (isset($_POST['btn-send'])) {
 
     $mailheader = "From: " . $name . "<" . $email . ">\r\n";
 
+    $finalmessage = "Name: " . $name . "\r\n Email = " . $email . " \r\n Message = " . $message;
+
     $recipient = "kamal@neccedu.com";
     if (mail($recipient, $subject, $message, $mailheader)) {
         header("location:index.php?success");

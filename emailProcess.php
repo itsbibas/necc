0bf4f841b23bf3f49af1f4e0ff3830dbbb4f126e
iamdurlove <durlove60@gmail.com>
2022-08-18 21:59:41 +0545

    final contact form update

--- emailProcess.php
+++ emailProcess.php
@@ -5,12 +5,10 @@ if (isset($_POST['btn-send'])) {
     $subject = "Website contact us form fill notice";
     $message = $_POST['message'];
 
-    $finalmessage = " From: " + $name + "Email: " + $email + "Message: " + $message;
-
     $mailheader = "From: " . $name . "<" . $email . ">\r\n";
 
     $recipient = "kamal@neccedu.com";
-    if (mail($recipient, $subject, $finalmessage, $mailheader)) {
+    if (mail($recipient, $subject, $message, $mailheader)) {
         header("location:index.php?success");
     }
 } else {

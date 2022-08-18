c3261c33325b66e81cef2de1b1eed676b3ce6938
iamdurlove <durlove60@gmail.com>
2022-08-18 22:50:49 +0545

    conntact and enquiry update, blog small change

--- blogs.php
+++ blogs.php
@@ -12,6 +12,14 @@ include './include/section-flex.php';
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0" nonce="FkACR5g1"></script>
 
-<div class="fb-page" data-href="https://www.facebook.com/NECCedu.Consultancy" data-tabs="timeline" data-width="100%" data-height="1000" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+<div class="fb-page" data-href="https://www.facebook.com/NECCedu.Consultancy" data-tabs="timeline" data-width="300" data-height="1000" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
     <blockquote cite="https://www.facebook.com/NECCedu.Consultancy" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/NECCedu.Consultancy">NECC Educational Consultancy</a></blockquote>
 </div>
+
+
+<style>
+    .fb-page {
+        width: 100%;
+        margin: auto;
+    }
+</style>
